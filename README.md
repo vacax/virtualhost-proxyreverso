2f5a378e88fcde10b0279ba92e6018b453d164f9
Carlos Camacho <ccamachog@avathartech.com>
2024-02-14 22:49:33 -0400

    Mejorando los scripts para trabajar los archivos para trabajar en el proxy reverso.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ En una instancia basada en Amazon Linux, clonar el proyecto actual y ejecutar lo
 ### Ubuntu:
 
 `
-wget https://raw.githubusercontent.com/vacax/virtualhost-proxyreverso/master/basico-ubuntu.sh && chmod +x basico-ubuntu.sh && bash basico-ubuntu.sh && git clone https://github.com/vacax/virtualhost-proxyreverso && cd virtualhost-proxyreverso
+wget https://raw.githubusercontent.com/vacax/virtualhost-proxyreverso/master/basico-ubuntu.sh && chmod +x basico-ubuntu.sh && bash basico-ubuntu.sh
 `
 
 ### Amazon Linux 2:
@@ -41,11 +41,11 @@ free
 
 En este punto tenemos disponible todas las herramientas necesarias instaladas.
 
-**Configurando Virtualhost**
+### **Configurando Virtualhost**
 
 Es necesario contar con un servicio DNS, para poder registrar los registro tipo A, 
 apuntando a la dirección IP de la máquina asignada en Amazon. En el archivo ``virtualhost.conf`` 
-en la ruta de ``/etc/httpd/conf.d/``, deben cambio el valor ``CAMBIAR`` por la IP, ver imagenes.
+en la ruta de ``/etc/apache2/sites-available/``, deben cambio el valor ``CAMBIAR`` por la IP, ver imágenes.
 
 ![amazon-ip](imagenes/ip-amazo.png)
 
@@ -53,6 +53,27 @@ en la ruta de ``/etc/httpd/conf.d/``, deben cambio el valor ``CAMBIAR`` por la I
 
 Una vez modificado ejecutar el comando: 
 ```
-sudo service httpd reload
+sudo service apache2 reload
 ```
 
+### **Configurando Proxy Reverso**
+
+Ver el archivo ``/etc/apache2/sites-available/proxyreverso.conf``, y sustituir el valor ``CAMBIAR``. Ver imagen.
+
+![Proxy Reverso](imagenes/proxyreverso.png)
+
+Una vez modificado ejecutar el comando:
+```
+sudo service apache2 reload
+```
+
+### **Configurando Proxy Reverso y Seguridad**
+
+Ver el archivo ``/etc/apache2/sites-available/seguro.conf``, y sustituir el valor ``CAMBIAR``. Ver imagen.
+
+![Seguridad y Proxy](imagenes/seguridad.png)
+
+Una vez modificado ejecutar el comando:
+```
+sudo service apache2 reload
+```
--- basico-ubuntu.sh
+++ basico-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Instalando estructura basica para clase virtualhost y proxy reverso"
 
-# Habilitando la memoria de intecambio.
+# Habilitando la memoria de intercambio.
 sudo dd if=/dev/zero of=/swapfile count=2048 bs=1MiB
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -10,12 +10,40 @@ sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 # Instando los software necesarios para probar el concepto.
-sudo apt update && sudo apt -y install zip unzip nmap apache2 certbot
+sudo apt update && sudo apt -y install zip unzip nmap apache2 certbot tree
 
 # Instalando la versión sdkman y java
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java
+
+# Utilizando la versión de java 17 como base.
+sdk install java 17.0.9-tem
 
 # Subiendo el servicio de Apache.
 sudo service apache2 start
+
+# Clonando el repositorio.
+git clone https://github.com/vacax/virtualhost-proxyreverso
+
+# Copiando los archivos de configuración en la ruta indicada.
+sudo cp ~/virtualhost-proxyreverso/configuraciones/virtualhost.conf /etc/apache2/sites-available/
+sudo cp ~/virtualhost-proxyreverso/configuraciones/seguro.conf /etc/apache2/sites-available/
+sudo cp ~/virtualhost-proxyreverso/configuraciones/proxyreverso.conf /etc/apache2/sites-available/
+
+# Creando las estructuras de los archivos.
+sudo mkdir -p /var/www/html/app1 /var/www/html/app2
+
+# Creando los archivos por defecto.
+printf "<h1>Sitio Aplicacion #1</h1>" | sudo tee /var/www/html/app1/index.html
+printf "<h1>Sitio Aplicacion #2</h1>" | sudo tee /var/www/html/app2/index.html
+
+# Clonando el proyecto ORM y moviendo a la carpeta descargada.
+cd ~/
+git clone https://github.com/vacax/orm-jpa
+cd orm-jpa
+
+# Ejecutando la creación de fatjar
+./gradlew shadowjar
+
+# Subiendo la aplicación puerto por defecto.
+java -jar ~/orm-jpa/build/libs/app.jar > ~/orm-jpa/build/libs/salida.txt 2> ~/orm-jpa/build/libs/error.txt &
